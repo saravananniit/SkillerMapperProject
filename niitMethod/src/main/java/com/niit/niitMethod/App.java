b3ee26881f53e52191c3fe81ad37ebdc47be7b2f
Global.Root <Global.Root@CHN-NH-PSARAVAN.in.niit.com>
2018-02-11 19:06:51 +0530

    integration between quickstart and webapp demo
    refer -> niitMethod proj and niitWeb proj using maven

---
+++ niitMethod/src/main/java/com/niit/niitMethod/App.java
@@ -0,0 +1,18 @@
+package com.niit.niitMethod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+        
+        Hello obj=new Hello();
+        System.out.println(obj.call("mani"));
+        
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ niitMethod/src/main/java/com/niit/niitMethod/Hello.java
@@ -0,0 +1,13 @@
+package com.niit.niitMethod;
+
+public class Hello {
+	
+	private String name=null;
+	public String call(String x)
+	{
+		name=x;
+		return "Welcome"+name+"to niit";
+		
+	}
+
+}
