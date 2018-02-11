7df391ac1e2aaa80eed4271ad96d5e024c997928
Global.Root <Global.Root@CHN-NH-PSARAVAN.in.niit.com>
2018-02-11 19:08:14 +0530

    using core java main method to store in H2 database code

---
+++ dbtest/src/main/java/dbtest/testing.java
@@ -0,0 +1,45 @@
+package dbtest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection connection = null;
+//		ResultSet resultSet = null;
+		Statement statement = null;
+
+		try {
+			System.out.println("welcome");
+		Class.forName("org.h2.Driver");
+			connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/./sample/test", "sa", "");
+			statement = connection.createStatement();
+			String query="insert into test values(568,'rama')";
+			//resultSet = statement.executeQuery("SELECT EMPNAME FROM EMPLOYEEDETAILS");
+			//while (resultSet.next()) {
+				//System.out.println("EMPLOYEE NAME:"
+					//	+ resultSet.getString("EMPNAME"));
+			//}
+			
+			statement.execute(query);
+			System.out.println("sucess");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+			//	resultSet.close();
+				statement.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	}
+
+
