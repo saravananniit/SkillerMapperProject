59f560980c40ee4ce53457c0236505adde777654
Global.Root <Global.Root@CHN-NH-PSARAVAN.in.niit.com>
2018-02-11 19:05:13 +0530

    integration between quickstart and web app using simple API

---
+++ niitWeb/src/main/java/working/people.java
@@ -0,0 +1,47 @@
+package working;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.niit.niitMethod.*;
+
+/**
+ * Servlet implementation class people
+ */
+public class people extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       private Hello obj=null;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public people() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+obj=new Hello();
+String w=obj.call("saravanan");
+response.getWriter().append(w);
+		response.getWriter().append("done");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ niitWeb/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<a href="people">click</a>
+</body>
+</html>
