a8424389c7cfc9af3bdb2766b328f405603f9f18
Global.Root <Global.Root@CHN-NH-PSARAVAN.in.niit.com>
2018-02-11 19:09:23 +0530

    Using Web app to store data at H2 database

---
+++ firstdemo/src/main/java/com/apple/ram/one.java
@@ -0,0 +1,71 @@
+package com.apple.ram;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class one
+ */
+public class one extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public one() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+							
+		Connection connection = null;
+		//ResultSet resultSet = null;
+		Statement statement = null;
+
+		try {
+			System.out.println("welcome");
+		Class.forName("org.h2.Driver");
+			connection = DriverManager.getConnection("jdbc:h2:tcp://localhost/./sample/test", "sa", "");
+			statement = connection.createStatement();
+			String query="insert into test values(0178,'james bond')";
+			
+			statement.execute(query);
+			System.out.println("sucess");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				statement.close();
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ firstdemo/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<a href="one">click</a>
+</body>
+</html>
